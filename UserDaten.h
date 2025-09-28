25872b47da4014d9dbc77aea4b6ce44ec31a7b5f
CarAndMore <44379612+CarAndMore@users.noreply.github.com>
2025-09-28 15:42:17 +0200

    Create UserDaten.h

---
+++ UserDaten.h
@@ -0,0 +1,15 @@
+// WLAN 1
+#define STASSID "wlan1"            // SSID
+#define STAPSK "12345678"          // Passwort
+
+// WLAN 2
+#define STASSID2 "wlan2"           // Alternative SSID
+#define STAPSK2 "12345678"         // Passwort
+
+// Access Point
+#define APNAME "Spieluhr"          // Name des AP
+#define APPSK "ABCD1234"           // Passwort des AP
+
+// OTA-Konfiguration
+#define HOSTNAME "spieluhr-1"      // Hostname für OTA
+#define otaPASSWD "0987654321123"  // Passwort für OTA
